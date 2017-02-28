43643c11172ec0c25154826cb44024feedfdbde1
rfamg <rafaelmg@brandeis.edu>
2017-02-28 17:36:47 -0500

    Add files via upload

---
+++ script/test.R
@@ -0,0 +1,28 @@
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#  Capital one Finance Financial Dossier
+#  Written by Rafael Guimaraees
+#  Date 27th February 2017
+#  Version 1.0
+#  ---------------------------------------------------------------------------
+#  ---------------------------------------------------------------------------
+
+
+
+library(ggplot2)
+library(xts)
+library(dygraphs)
+library(RCurl)  # functions for gathering data from the web
+library(XML)  # XML and HTML parsing
+library(quantmod) # use for gathering and charting economic data
+# online documentation for quantmod at <http://www.quantmod.com/> 
+library(Quandl)  # extensive financial data online
+# online documentation for Quandl at <http://www.quandl.com/> 
+library(lubridate) # date functions
+library(zoo)  # utilities for working with time series
+library(xts)  # utilities for working with time series
+library(forecast)
+library(gtable)
+library(rvest)
+library(grid)
+library(extrafont)
